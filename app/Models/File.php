1693d307d6a72e8f447ac4ef3953d0bf5b1de4aa
StackOverfloweds <bryangithub32@gmail.com>
2026-08-04 22:19:30 +0800

    feat: implement Breeze auth with user UUID and storage fix

---
+++ app/Models/File.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class File extends Model
+{
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+    public function parent() {
+        return $this->belongsTo(File::class, 'parent_id');
+    }
+    public function children() {
+        return $this->hasMany(File::class, 'parent_id');
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,16 +6,17 @@ namespace App\Models;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'username','email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasUuids;
 
     /**
      * Get the attributes that should be cast.
@@ -28,5 +29,9 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function files () {
+        return $this->hasMany(File::class);
     }
 }
